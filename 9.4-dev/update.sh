62b88cff5123d30ab31f3b393be1e2a3c79f54ab
Charles Oliver Nutter <headius@headius.com>
2025-06-26 11:18:51 -0500

    Update snapshots to new central URL

--- 9.4-dev/update.sh
+++ 9.4-dev/update.sh
@@ -5,12 +5,12 @@
 
 	JRUBY_VERSION=$(< VERSION)
 	JRUBY_SNAPSHOT_VERSION=${JRUBY_VERSION}-SNAPSHOT
-	JRUBY_SNAPSHOT_XML=$(curl https://oss.sonatype.org/content/repositories/snapshots/org/jruby/jruby-dist/${JRUBY_SNAPSHOT_VERSION}/maven-metadata.xml)
+	JRUBY_SNAPSHOT_XML=$(curl https://central.sonatype.com/repository/maven-snapshots/org/jruby/jruby-dist/${JRUBY_SNAPSHOT_VERSION}/maven-metadata.xml)
 	JRUBY_SNAPSHOT_TIMESTAMP=$(echo ${JRUBY_SNAPSHOT_XML} | xmllint --xpath "metadata/versioning/snapshot/timestamp/text()" -)
 	JRUBY_SNAPSHOT_BUILDNUMBER=$(echo ${JRUBY_SNAPSHOT_XML} | xmllint --xpath "metadata/versioning/snapshot/buildNumber/text()" -)
 	JRUBY_SNAPSHOT_FULLVERSION=${JRUBY_VERSION}-${JRUBY_SNAPSHOT_TIMESTAMP}-${JRUBY_SNAPSHOT_BUILDNUMBER}
-	echo https://oss.sonatype.org/content/repositories/snapshots/org/jruby/jruby-dist/${JRUBY_SNAPSHOT_VERSION}/jruby-dist-${JRUBY_SNAPSHOT_FULLVERSION}-bin.tar.gz
-	SHA="$(curl -fSL https://oss.sonatype.org/content/repositories/snapshots/org/jruby/jruby-dist/${JRUBY_SNAPSHOT_VERSION}/jruby-dist-${JRUBY_SNAPSHOT_FULLVERSION}-bin.tar.gz | sha256sum | awk '{print $1}')"
+	echo https://central.sonatype.com/repository/maven-snapshots/org/jruby/jruby-dist/${JRUBY_SNAPSHOT_VERSION}/jruby-dist-${JRUBY_SNAPSHOT_FULLVERSION}-bin.tar.gz
+	SHA="$(curl -fSL https://central.sonatype.com/repository/maven-snapshots/org/jruby/jruby-dist/${JRUBY_SNAPSHOT_VERSION}/jruby-dist-${JRUBY_SNAPSHOT_FULLVERSION}-bin.tar.gz | sha256sum | awk '{print $1}')"
 
 	for i in $(ls); do
 		if [ -d "$i" ]; then
